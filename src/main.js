45eb1c14e1a4a97fa9830b61b4a93d2bc7d7e3c0
duongbaonam <namb1910413@student.ctu.edu.vn>
2022-11-02 07:09:51 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap'
+import router from './router'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
